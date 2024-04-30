4a41c87ea7982666b7980e74a719aee7024a830b
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-04-30 09:10:59 +0700

    add home page footer and navar

--- src/script/layout/footer.js
+++ src/script/layout/footer.js
@@ -1,9 +1,26 @@
-const btn = document.getElementById("btn1");
-const arrow = document.getElementById("arrow1");
-const container = document.getElementById("container1");
-
-    function dropDown () {
-        arrow.classList.toggle("rotate-180");
-        container.classList.toggle("hidden")
-    }
-    btn.addEventListener("click", dropDown)
+// Footer
+const footer= document.querySelector(".footer");
+fetch("../../component/footer.html")
+  .then((res) => res.text())
+  .then((data) => {
+    footer.innerHTML = data;
+    
+    const btn = document.querySelectorAll(".btn");
+    const container = document.querySelectorAll(".container");
+    const upArrow = document.querySelectorAll(".arrow");
+  
+    for (let i = 0; i < btn.length; i++) {
+      btn[i].addEventListener("click", () => {
+        btn[i].classList.toggle("active");
+        if (btn[i].classList.contains("active")) {
+          container[i].classList.toggle("hidden");
+          upArrow[i].classList.add("rotate-180");
+        } else {
+          container[i].classList.remove("max-h-screen");
+          container[i].classList.add("max-h-0");
+          upArrowArrow[i].classList.remove("rotate-180");
+        }
+      });
+    }    
+  })
+  .catch((error) => console.error("Error fetching included file:", error));
--- src/script/layout/layout.js
+++ src/script/layout/layout.js
@@ -1,2 +1,1 @@
-import * as navbar from "./navbar.js";
 import * as footer from "./footer.js";
