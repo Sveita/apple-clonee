b65200cbbafbaa390081156f9f13ea61a1fa266b
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-05-08 10:32:15 +0700

    feature : add page ipad

---
+++ src/script/ipad.js
@@ -0,0 +1,7 @@
+const ipad = document.querySelector(".ipad");
+fetch("../component/ipad/ipad.html")
+  .then((res) => res.text())
+  .then((data) => {
+    ipad.innerHTML = data;
+  })
+  .catch((error) => console.error("Error fetching included file:", error));
