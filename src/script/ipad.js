db9221c74fb2f09a413ea8ac3397b54a8d7baed0
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-05-28 15:14:54 +0700

    editing ipad page

--- src/script/ipad.js
+++ src/script/ipad.js
@@ -3,5 +3,6 @@ fetch("../component/ipad/ipad.html")
   .then((res) => res.text())
   .then((data) => {
     ipad.innerHTML = data;
+
   })
   .catch((error) => console.error("Error fetching included file:", error));
