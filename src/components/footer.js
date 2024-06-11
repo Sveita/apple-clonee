d22eed4713d1779060fc93261216cc1226250a96
AnB School 0026 <anbschool0026@AnBs-Mac-mini-157.local>
2024-06-11 13:35:04 +0700

    footer : move footer,.js in the components folder

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,24 +4,6 @@ fetch("../component/layout/footer.html")
   .then((res) => res.text())
   .then((data) => {
     footer.innerHTML = data;
-    
-    // const btn = document.querySelectorAll(".btn");
-    // const container = document.querySelectorAll(".container");
-    // const upArrow = document.querySelectorAll(".arrow");
-  
-    // for (let i = 0; i < btn.length; i++) {
-    //   btn[i].addEventListener("click", () => {
-    //     btn[i].classList.toggle("active");
-    //     if (btn[i].classList.contains("active")) {
-    //       container[i].classList.toggle("hidden");
-    //       upArrow[i].classList.add("rotate-180");
-    //     } else {
-    //       container[i].classList.remove("max-h-screen");
-    //       container[i].classList.add("max-h-0");
-    //       upArrowArrow[i].classList.remove("rotate-180");
-    //     }
-    //   });
-    // }    
 // 
     const shop = document.getElementById('shopButton')
     const shopContainer = document.getElementById('shopContainer')
